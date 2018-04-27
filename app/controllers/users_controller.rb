3803f5d4cf1491c33824f0b684bf1d4493d85586
a1851yl <a1851yl@aiit.ac.jp>
2018-04-27 08:25:06 +0000

    finish ch_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   
+  
   def show
    @user = User.find(params[:id])
    
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,23 @@ Rails.application.routes.draw do
 
   
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   
-  get '/help'    , to: 'static_pages#help'
+  get '/help'     , to: 'static_pages#help'
   
-  get '/about'   , to: 'static_pages#about'
+  get '/about'    , to: 'static_pages#about'
   
-  get '/contact' , to: 'static_pages#contact'
+  get '/contact'  , to: 'static_pages#contact'
   
-  get '/signup'  , to: 'users#new'
+  get '/signup'   , to: 'users#new'
+  
+  get '/login'    , to: 'sessions#new'
+  
+  post '/login'   , to: 'sessions#create'
+  
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   
